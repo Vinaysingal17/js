1a41eb04aaddef04ef843ebf3900cfe0dcf95572
Vinaysingal17 <vsingal212@gmail.com>
2026-01-27 23:50:21 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" //treat all js code as newer version
+
+let name = "vinay"
+let age = 18
+let accountLoggedin = true
+
+console.table[name, age, accountLoggedin]
+//string ==> use into double coats
+//number 
+//bigint
+//null ==> standalone value
+// undefined ==> i will defined it later just putting the name not value. let = state;
+//symbol ==> unique
+
+
+//object
+
+console.log(typeof age);
