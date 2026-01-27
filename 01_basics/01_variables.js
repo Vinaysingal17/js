fa1673c8ea81e48904d1f12d0dde91eb1c1cdd5c
Vinaysingal17 <vsingal212@gmail.com>
2026-01-27 23:49:53 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountName = "vinaysingal"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+accountName = "vinay"
+accountPassword = "89098"
+accountCity = "Delhi"
+
+console.table ([accountName, accountPassword, accountCity]);
